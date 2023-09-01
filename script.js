bfb6080a270fcafe6c7d96591827e8bca17ec868
Quartezz <kamil.machel03@gmail.com>
2023-09-01 23:17:59 +0200

    Added base variables as display Controller function

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const Player = (sign) => {
+    this.sign = sign;
+
+    const getSign = () => {
+        return sign;
+    };
+
+    return {getSign};
+};
+
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return { setField, getField, reset};
+})();
+
+const displayController = (() => {
+    const fieldElements = document.querySelectorAll(".field");
+    const messageElement = document.getElementById("message");
+    const restartButton = document.getElementById("restart-btn");
+})
